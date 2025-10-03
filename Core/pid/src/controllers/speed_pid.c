a1e0ff93f9a393452b4ec19bad45f02e57e563f4
Lucas Huguenin <l-huguenin@hotmail.com>
2025-10-03 08:26:24 -0800

    Feat: PWM clamping for motor control when using the base speed PID, to account for mechanical differences between motors

--- Core/pid/src/controllers/speed_pid.c
+++ Core/pid/src/controllers/speed_pid.c
@@ -10,6 +10,8 @@
 #define KD 0
 #define FRAME_INTERVAL 10  // ms
 
+#define PWM_MAX_DELTA 100
+
 static BaseSpeedPid base_pid = {
     .kp = KP,
     .ki = KI,
@@ -27,8 +29,10 @@ static struct {
     int16_t right_i;
     int16_t left_d;
     int16_t right_d;
-    int16_t left_pid;
-    int16_t right_pid;
+    int16_t last_left_pwm;
+    int16_t last_right_pwm;
+    int16_t left_pwm;
+    int16_t right_pwm;
 } pid_struct = {0};
 
 static inline void update_p(void) {
@@ -56,15 +60,24 @@ static inline void update_d(void) {
         base_pid.kd * speed_errors->right_delta_error / base_pid.frame_interval;
 }
 
-static inline void update_pid(void) {
-    update_p();
-    update_i();
-    update_d();
-
-    pid_struct.left_pid =
+static inline void update_pwm(void) {
+    const int16_t left_out =
         pid_struct.left_p + pid_struct.left_i + pid_struct.left_d;
-    pid_struct.right_pid =
+    const int16_t right_out =
         pid_struct.right_p + pid_struct.right_i + pid_struct.right_d;
+
+    const int16_t left_delta = left_out - pid_struct.last_left_pwm;
+    const int16_t right_delta = right_out - pid_struct.last_right_pwm;
+
+    pid_struct.left_pwm = left_delta > PWM_MAX_DELTA
+                              ? pid_struct.last_left_pwm + PWM_MAX_DELTA
+                              : left_out;
+    pid_struct.right_pwm = right_delta > PWM_MAX_DELTA
+                               ? pid_struct.last_right_pwm + PWM_MAX_DELTA
+                               : right_out;
+
+    pid_struct.last_left_pwm = pid_struct.left_pwm;
+    pid_struct.last_right_pwm = pid_struct.right_pwm;
 }
 
 void init_base_speed_pid(const ErrorStruct* error_struct) {
@@ -74,10 +87,13 @@ void init_base_speed_pid(const ErrorStruct* error_struct) {
 const BaseSpeedPid* get_base_speed_pid_ptr(void) { return &base_pid; }
 
 void get_base_speed_pid(int16_t* left_pwm, int16_t* right_pwm) {
-    update_pid();
+    update_p();
+    update_i();
+    update_d();
+    update_pwm();
 
-    if (left_pwm != NULL) *left_pwm = pid_struct.left_pid;
-    if (right_pwm != NULL) *right_pwm = pid_struct.right_pid;
+    if (left_pwm != NULL) *left_pwm = pid_struct.left_pwm;
+    if (right_pwm != NULL) *right_pwm = pid_struct.right_pwm;
 }
 
 bool update_pending_base_speed_pid(void) {
